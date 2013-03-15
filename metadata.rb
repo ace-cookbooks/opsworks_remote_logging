55a249a6e64b5437d359c8243cb27e368c3190e9
Ryan Schlesinger <ryan@instanceinc.com>
2013-03-15 15:48:28 -0700

    Initial commit

---
+++ metadata.rb
@@ -0,0 +1,8 @@
+name             "opsworks_remote_logging"
+maintainer       "Ryan Schlesinger"
+maintainer_email "ryan@instanceinc.com"
+license          "Apache 2.0"
+description      "Sets up remote logging with rsyslog"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          "0.1.0"
+
